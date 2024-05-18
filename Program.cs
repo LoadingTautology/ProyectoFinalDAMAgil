4a9c0303f3c2b58f46887498797b0405e4a48123
Marc Catalan Alfonso <mebucles@gmail.com>
2024-05-18 20:48:36 +0200

    Implementacion de idiomas y selector de idiomas

--- Program.cs
+++ Program.cs
@@ -1,70 +1,82 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using ProyectoFinalDAMAgil.Scaffold;
+using System.Globalization;
 
 namespace ProyectoFinalDAMAgil
-{ 
+{
     public class Program
     {
         public static void Main(string[] args)
         {
-			var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews()
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization();
 
-			/* ********* Inyeccion Contexto ********* */
-			var connectionString = builder.Configuration.GetConnectionString("Default");
-			builder.Services.AddDbContext<DbappProyectoFinalContext>(options => { options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)); });
+            // Localization configuration
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-			/* ********* Inyeccion dependencias Servicios********* */
-			builder.Services.AddScoped<Services.Usuario.IUsuarioService, Services.Usuario.UsuarioService>();
+            // ********* Inyeccion Contexto *********
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+            builder.Services.AddDbContext<DbappProyectoFinalContext>(options =>
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+            // ********* Inyeccion dependencias Servicios *********
+            builder.Services.AddScoped<Services.Usuario.IUsuarioService, Services.Usuario.UsuarioService>();
             builder.Services.AddScoped<Services.Correoelectronico.ICorreoelectronicoService, Services.Correoelectronico.CorreoelectronicoService>();
             builder.Services.AddScoped<Services.Administrador.IAdministradorService, Services.Administrador.AdministradorService>();
             builder.Services.AddScoped<Services.Centroeducativo.ICentroeducativoService, Services.Centroeducativo.CentroeducativoService>();
             builder.Services.AddScoped<Services.Usuarioscentroeducativo.IUsuarioscentroeducativoService, Services.Usuarioscentroeducativo.UsuarioscentroeducativoService>();
 
-
-
-
+            // Authentication configuration
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
-			{
-				options.LoginPath = "/Login/IniciarSesion";
-				options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-				options.AccessDeniedPath = "/Login/IniciarSesion";
-			});
+            {
+                options.LoginPath = "/Login/IniciarSesion";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                options.AccessDeniedPath = "/Login/IniciarSesion";
+            });
 
-			builder.Services.AddRazorPages();
+            builder.Services.AddRazorPages();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-			
-			app.UseHttpsRedirection();/*  */
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthorization();
+            app.UseAuthentication(); // Ensure authentication middleware is used
+            app.UseAuthorization();
 
-			app.MapControllerRoute(
-				name: "default",
+            // Localization middleware configuration
+            var supportedCultures = new[] { new CultureInfo("es"), new CultureInfo("en") };
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("es"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+            app.MapControllerRoute(
+                name: "default",
                 pattern: "{controller=MainPage}/{action=Index}/{id?}");
 
             app.Run();
-		}
-
-	}
+        }
+    }
 }
-
-
-//"ConnectionStrings": {
-//  "Default": "server=database.proyectofinaldam2324.site;port=3306;database=proyecto;uid=root;password=pedrolachuspa"
-//}
-
-//Scaffold-DbContext "server=localhost;port=3306;database=DBAppProyectoFinal;uid=root;password=" Pomelo.EntityFrameworkCore.MySql -NoOnConfiguring -OutputDir Scaffold -Force
