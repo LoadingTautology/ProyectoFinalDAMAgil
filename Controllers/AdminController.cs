19e9efcd02841c051191e31db128a84f31189346
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-22 23:55:03 +0200

    editar aulas solucionado

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -508,10 +508,11 @@ namespace ProyectoFinalDAMAgil.Controllers
             {
                 return View("~/Views/Admin/Aulas/Editar.cshtml", aulaModel);
             }
+            AulaModel aulaModelInicial = await _aulaService.ReadAula(idAula);
 
-            if (idAula != aulaModel.IdAula && await _aulaService.ExistAula(aulaModel))
+            if (aulaModelInicial.NumeroAula != aulaModel.NumeroAula &&  await _aulaService.ExistAula(aulaModel))
             {
-                ViewData["Mensaje"] = "AulaYaExiste";
+                ViewData["Mensaje"] = "Ese número de aula ya existe";
                 return View("~/Views/Admin/Aulas/Editar.cshtml", aulaModel);
             }
 
