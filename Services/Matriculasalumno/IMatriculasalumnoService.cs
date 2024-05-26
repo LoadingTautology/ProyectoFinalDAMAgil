7dcb12da2cb54d6437b843503add183ddc7b48b5
pedromarub.92@outlook.es <pedromarub.92@outlook.es>
2024-05-26 19:24:59 +0200

    base de datos actualizada

--- Services/Matriculasalumno/IMatriculasalumnoService.cs
+++ Services/Matriculasalumno/IMatriculasalumnoService.cs
@@ -4,24 +4,24 @@ namespace ProyectoFinalDAMAgil.Services.Matriculasalumno
 {
     public interface IMatriculasalumnoService
     {
-        Task<AsignaturasprofesorModel> CreateAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
+        //Task<AsignaturasprofesorModel> CreateAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
 
-        Task<AsignaturasprofesorModel> ReadAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
+        //Task<AsignaturasprofesorModel> ReadAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
 
-        Task<AsignaturasprofesorModel> DeleteAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
+        //Task<AsignaturasprofesorModel> DeleteAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
 
-        Task<bool> ExistAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
+        //Task<bool> ExistAsignaturasprofesor(int idProfesor, int idEstudio, int idAsignatura);
 
-        Task<IEnumerable<AsignaturasprofesorModel>> ListAsignaturasprofesor(int idProfesor);
+        //Task<IEnumerable<AsignaturasprofesorModel>> ListAsignaturasprofesor(int idProfesor);
 
-        Task<IEnumerable<HorarioModel>> ListHorariosProfesor(int idProfesor);
+        //Task<IEnumerable<HorarioModel>> ListHorariosProfesor(int idProfesor);
 
-        Task<IEnumerable<AsignaturaModel>> ListAsignaturasProfesor(int idProfesor);
-        Task<IEnumerable<CicloformativoModel>> ListEstudiosProfesor(int idProfesor);
+        //Task<IEnumerable<AsignaturaModel>> ListAsignaturasProfesor(int idProfesor);
+        //Task<IEnumerable<CicloformativoModel>> ListEstudiosProfesor(int idProfesor);
 
-        Task<IEnumerable<AulaModel>> ListAulasProfesor(int idProfesor);
+        //Task<IEnumerable<AulaModel>> ListAulasProfesor(int idProfesor);
 
-        Task<bool> ExistHorarioEnConflictoProfesor(int idProfesor, int idEstudio, int idAsignatura);
+        //Task<bool> ExistHorarioEnConflictoProfesor(int idProfesor, int idEstudio, int idAsignatura);
 
     }
 }
