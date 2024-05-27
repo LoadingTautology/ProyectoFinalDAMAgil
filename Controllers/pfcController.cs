22480f43ecfe59f37bdc022d184555cf0c6cd2a8
Marc Catalan Alfonso <mebucles@gmail.com>
2024-05-27 22:17:01 +0200

    pfc > Grantt

---
+++ Controllers/pfcController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFinalDAMAgil.Controllers
+{
+    public class pfcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Grantt()
+        {
+            return View("~/Views/pfc/Grantt.cshtml");
+        }
+    }
+}
